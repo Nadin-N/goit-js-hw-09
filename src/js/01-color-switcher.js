6bad09c100a0524e894ca74625f9d69bf8023169
Nadin Severin <nadinlikes2018@gmail.com>
2023-01-10 00:50:00 +0200

    add 1th and 2nd tasks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
+let timerId = null;
 
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick(e) {
+  if (!refs.startBtn.disabled) {
+    refs.startBtn.disabled = true;
+  }
+  timerId = setInterval(
+    e => (refs.bodyEl.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+}
+
+function onStopClick(e) {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
